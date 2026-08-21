a82784c64b6e4236ac8bc8bd953c7ffbf71a7bdc
archlinux <stefanwlindner@gmail.com>
2026-08-21 21:14:36 +0200

    Update Activ-Win.ps1

--- Activ-Win.ps1
+++ Activ-Win.ps1
@@ -1,4 +1,4 @@
-echo  Dev: massgrave
-echo  Website: massgrave.dev
-echo  Thanks for using https://github.com/archlinux-dev/win10!
+echo  "Dev: massgrave"
+echo  "Website: massgrave.dev"
+echo  "Thanks for using https://github.com/archlinux-dev/win10!"
 irm get.activated.win | iex

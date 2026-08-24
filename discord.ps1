f43f7033013b14c2ecb629d3d5ae71c281ba74e8
archlinux <stefanwlindner@gmail.com>
2026-08-24 04:17:57 +0200

    Update discord.ps1

--- discord.ps1
+++ discord.ps1
@@ -1,1 +1,4 @@
+echo  "Dev: Jason Citron & Stanislav Vishnevskiy"
+echo  "Website: https://discord.com/download"
+echo  "Thanks for using https://github.com/archlinux-dev/win10!"
 winget install Discord.Discord

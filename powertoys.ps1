5ce74adb97243600e1d738bb7c5e1aa7c3df4a1b
archlinux <stefanwlindner@gmail.com>
2026-08-24 04:16:22 +0200

    Update powertoys.ps1

--- powertoys.ps1
+++ powertoys.ps1
@@ -1,1 +1,4 @@
+echo  "Dev: Microsoft Corp."
+echo  "Website: https://github.com/microsoft/PowerToys/releases/"
+echo  "Thanks for using https://github.com/archlinux-dev/win10!"
 winget install Microsoft.PowerToys

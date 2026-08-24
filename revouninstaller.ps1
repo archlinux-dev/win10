aeb1cdc776b0b65b7c4f186554f4873796491a05
archlinux <stefanwlindner@gmail.com>
2026-08-24 04:15:14 +0200

    Add informational messages to revouninstaller.ps1

--- revouninstaller.ps1
+++ revouninstaller.ps1
@@ -1,1 +1,4 @@
+echo  "Dev: VS Revo Group"
+echo  "Website: https://www.revouninstaller.com/revo-uninstaller-free-download/"
+echo  "Thanks for using https://github.com/archlinux-dev/win10!"
 winget install RevoUninstaller.RevoUninstaller

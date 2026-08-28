236f54bea8f86bf4c567e50f4e1e08febcb864d9
archlinux <stefanwlindner@gmail.com>
2026-08-28 22:46:33 +0200

    Add system information script with installation instructions

---
+++ sysinfo.ps1
@@ -0,0 +1,5 @@
+echo  "Dev(s): https://systeminformer.com/about"
+echo  "Website: https://systeminformer.com/"
+echo  "Thanks for using https://github.com/archlinux-dev/win10!"
+
+winget install -e --id WinsiderSS.SystemInformer

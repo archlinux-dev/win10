748d6d2fabdaa202331d1df90980a8645dbde1fb
archlinux <stefanwlindner@gmail.com>
2026-09-17 16:24:03 +0200

    Disable Defender remover script execution
    Comment out the download and execution of the Defender remover executable due to script issues.

--- removedefender.ps1
+++ removedefender.ps1
@@ -2,5 +2,7 @@ echo  "Dev: ionuttbara"
 echo  "Website: github.com/ionuttbara/"
 echo  "Thanks for using https://github.com/archlinux-dev/win10!"
 
-Invoke-WebRequest -Uri "https://github.com/ionuttbara/windows-defender-remover/releases/download/release13-rev1/Defender.Remover.13.exe" -OutFile "$env:TEMP\Defender.Remover.13.exe"
-Start-Process "$env:TEMP\Defender.Remover.13.exe" -Verb RunAs
+echo "This Script is currently broken!! Please try again later!!!
+
+#Invoke-WebRequest -Uri "https://github.com/ionuttbara/windows-defender-remover/releases/download/release13-rev1/Defender.Remover.13.exe" -OutFile "$env:TEMP\Defender.Remover.13.exe"
+#Start-Process "$env:TEMP\Defender.Remover.13.exe" -Verb RunAs
